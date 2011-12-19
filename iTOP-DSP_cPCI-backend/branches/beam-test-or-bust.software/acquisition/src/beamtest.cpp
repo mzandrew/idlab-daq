5b951fa481896e0a33148655e2007e3949a40449
Matt Andrew <mzandrew@users.noreply.github.com>
2011-12-19 16:29:27 +0000

    actually put some thought into how beamtest.cpp might work

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/beamtest.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/beamtest.cpp
@@ -8,30 +8,51 @@
 #include "acquisition.h"
 
 int main(void) {
-	unsigned long int total_number_of_quarter_events_to_read_per_fiber_channel = 0;
-
+	// setup:
 	parse_config_file(".config");
 	setup_pci(card_id);
-	should_soft_trigger = true;
 	readout_all_pending_data();
 	setup_log_filenames_for_fiber();
 	open_files_for_all_enabled_fiber_channels();
-
 	unsigned short int beginning_window = 0;
 	unsigned short int ending_window = 63;
+	set_start_and_end_windows(beginning_window, ending_window);
+	usleep(50000);
+
+	// testing:
+	should_soft_trigger = true;
+
+	// actual running:
 	while (1) {
-		usleep(4000000);
-		for (int i=0; i<40; i++) {
+		wait_for_start_of_spill();
+		while (spill_is_active()) {
 			readout_N_events(1);
-			usleep(15000);
+			send_front_end_trigger_veto_clear();
+			reset_trigger_flip_flop();
 		}
-		// wait for spill completed signal
 		increment_spill_number_and_change_log_filenames_for_fiber();
+		usleep(250000);
+		sync();
 	}
 
+	// cleanup:
 	close_all_fiber_files();
 	close_pci();
-
 	return 0;
 }
+
+//	reset_trigger_flip_flop();
+//	send_front_end_trigger_veto_clear();
+//	wait_for_start_of_spill();
+//	while (spill_is_active()) {
+
+//	send_soft_trigger_request_command_packet();
+//	global_reset();
+//	clear_scaler_counters();
+//	set_all_DACs_to_built_in_nominal_values();
+
+//	set_start_and_end_windows(unsigned long int start_window, unsigned long int end_window);
+//	set_number_of_windows_to_look_back(unsigned long int look_back);
+//	open_files_for_output_and_read_N_events(unsigned long int N);
+//	setup_feedback_enables_and_goals(unsigned short int enable);
 
