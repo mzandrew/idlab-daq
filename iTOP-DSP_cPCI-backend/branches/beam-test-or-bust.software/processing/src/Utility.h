348000f70e100298b0b5699a85f75027b81b41f5
kurtisn <kurtisn@e3e55cb2-ede6-3e39-f4bf-95de71bb434d>
2011-12-23 22:57:56 +0000

    Significant changes to Raw->DST1 converter w/DQM to open and close many files, based on the logfile.

---
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/processing/src/Utility.h
@@ -0,0 +1,24 @@
+//Utility functions for parsing filenames, etc.
+#ifndef UTILITY__H
+#define UTILITY__H
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#define RAWDATA_PATH "/data/beamtest/rawdata"
+#define LOGFILE_NAME "logfile"
+#define DST_PATH "/data/beamtest/dst1"
+
+//Function to open the logfile for a given experiment
+void OpenLogFile(std::ifstream &logfile, unsigned int exp);
+
+//Function to find the next filename for the run of interest
+std::string NextRawFile(std::ifstream &logfile, unsigned int exp, unsigned int this_run, unsigned int &last_spill);
+
+//Generic program that searches for "spillXXXX" in the filename
+//and increments the number by one.  Mainly used to keep processing
+//more files without running a command for each one.
+std::string GetFilenameForNextSpill(std::string original_name);
+
+#endif
