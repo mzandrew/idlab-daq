9b4cad0f5510a58bfdf6d3c87286e5070c31b983
mzandrew <mzandrew@e3e55cb2-ede6-3e39-f4bf-95de71bb434d>
2011-12-23 03:27:59 +0000

    added prerun.cpp which resets some things, asks to wait for a keypress after the fiber links are up, and then behaves the same as shennanigans does (to take data for a while to the same file)

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/prerun.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/prerun.cpp
@@ -1,23 +1,35 @@
-// 2011-?? mza
+// 2011 mza
 
+using namespace std;
+
+#include <string>
+#include <stdio.h>
+#include <iostream>
 #include "pci.h"
 #include "fiber_readout.h"
 #include "command_packet_builder.h"
-#include "read_CAMAC.h" // this is for all non-3377 CAMAC stuff
-#include <stdio.h>
-#include <iostream>
+#include "read_CAMAC.h"
 #include "parse_config_file.h"
 #include "acquisition.h"
 #include "status_file.h"
-//#include "camac.h" // this is the 3377-specific readout
 
 int main(void) {
-	// setup:
 	parse_config_file(".config");
+	setup_pci(card_id);
+
+	global_reset();
+	usleep(350000);
+	cout << "press a key after all fiber links are up (steady green)" << endl;
+	getchar();
+	readout_all_pending_data();
+	clear_scaler_counters();
+	send_front_end_trigger_veto_clear();
+	reset_trigger_flip_flop();
+	set_all_DACs_to_built_in_nominal_values();
+	setup_feedback_enables_and_goals(1);
+
 	open_status_file_for_reading_and_writing();
 	read_status_file();
-	run_number++;
-	write_status_file();
 	create_directory_if_necessary(location_of_raw_datafiles);
 	generate_new_base_filename();
 	setup_pci(card_id);
@@ -37,8 +49,7 @@ int main(void) {
 	set_start_and_end_windows(beginning_window, ending_window);
 	usleep(50000);
 
-	// testing:
-//	should_soft_trigger = true;
+	should_soft_trigger = true;
 
 	// actual running:
 	while (1) {
@@ -49,30 +60,23 @@ int main(void) {
 //			CAMAC_read_3377s();
 			printf("\n");
 		}
-		increment_spill_number();
-		write_status_file();
-		generate_new_base_filename();
-		split_fiber_file_to_prepare_for_next_spill();
-		split_CAMAC_file_to_prepare_for_next_spill();
-		usleep(250000);
-		sync();
+//		increment_spill_number();
+//		write_status_file();
+//		generate_new_base_filename();
+//		split_fiber_file_to_prepare_for_next_spill();
+//		split_CAMAC_file_to_prepare_for_next_spill();
+//		usleep(250000);
+//		sync();
 	}
 
-	// cleanup:
 	close_all_fiber_files();
 	close_pci();
 	return 0;
 }
 
-//	reset_trigger_flip_flop();
-//	send_front_end_trigger_veto_clear();
 //	wait_for_start_of_spill();
 //	while (spill_is_active()) {
-
 //	send_soft_trigger_request_command_packet();
-//	global_reset();
-//	clear_scaler_counters();
-//	set_all_DACs_to_built_in_nominal_values();
 
 //	set_start_and_end_windows(unsigned long int start_window, unsigned long int end_window);
 //	set_number_of_windows_to_look_back(unsigned long int look_back);
