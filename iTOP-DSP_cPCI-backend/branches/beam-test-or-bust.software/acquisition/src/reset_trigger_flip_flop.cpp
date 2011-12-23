4eade78f7e085239ea549d1406b6b5b814fd4ea0
Matt Andrew <mzandrew@users.noreply.github.com>
2011-12-23 15:54:19 +0000

    minor change

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/reset_trigger_flip_flop.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/reset_trigger_flip_flop.cpp
@@ -8,13 +8,12 @@
 #include "acquisition.h"
 
 int main(void) {
-	unsigned long int total_number_of_quarter_events_to_read_per_fiber_channel = 0;
-
 	parse_config_file(".config");
 	setup_pci(card_id);
-	pci.sendVetoClear();
-	close_pci();
 
+	pci.sendVetoClear();
+
+	close_pci();
 	return 0;
 }
 
