253138f8f3ee235e4f3af12e68fb12ab1ac57d69
Matt Andrew <mzandrew@users.noreply.github.com>
2011-12-24 01:22:18 +0000

    tried to fix ftp push program; write command to selectively send commands to modules with a bitmask

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/turn_off_all_DACs.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/turn_off_all_DACs.cpp
@@ -14,6 +14,7 @@ int main(void) {
 	setup_pci(card_id);
 	should_soft_trigger = true;
 	readout_all_pending_data();
+	//set_some_DACs_to(0, 0xe);
 	set_all_DACs_to(0);
 	//set_all_DACs_to_built_in_nominal_values();
 	close_pci();
