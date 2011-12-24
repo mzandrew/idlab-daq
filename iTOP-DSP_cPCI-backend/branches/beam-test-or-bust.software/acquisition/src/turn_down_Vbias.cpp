cdd95a3c5ea9fc26213b6ae1e29b1f106c84f109
Matt Andrew <mzandrew@users.noreply.github.com>
2011-12-24 14:36:19 +0000

    turn on/off only Vbiases

---
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/turn_down_Vbias.cpp
@@ -0,0 +1,24 @@
+// 2011 mza
+
+using namespace std;
+
+#include <string>
+#include <stdio.h>
+#include "pci.h"
+#include "fiber_readout.h"
+#include "command_packet_builder.h"
+#include "read_CAMAC.h"
+#include "parse_config_file.h"
+#include "acquisition.h"
+
+int main(void) {
+	parse_config_file(".config");
+	setup_pci(card_id);
+
+	readout_all_pending_data();
+	send_command_packet_to_all_enabled_channels(0x5555b1a5, 0);
+
+	close_pci();
+	return 0;
+}
+
