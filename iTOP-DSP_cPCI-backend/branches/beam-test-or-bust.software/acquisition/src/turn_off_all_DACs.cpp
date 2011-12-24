2b9afd7d0b37895cd67c47ca1b69142647c84887
mzandrew <mzandrew@e3e55cb2-ede6-3e39-f4bf-95de71bb434d>
2011-12-24 14:46:52 +0000

    minor change

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/turn_off_all_DACs.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/turn_off_all_DACs.cpp
@@ -8,17 +8,16 @@
 #include "acquisition.h"
 
 int main(void) {
-	unsigned long int total_number_of_quarter_events_to_read_per_fiber_channel = 0;
-
 	parse_config_file(".config");
 	setup_pci(card_id);
 	should_soft_trigger = true;
 	readout_all_pending_data();
+
 	set_some_DACs_to(0, 0xe);
 	//set_all_DACs_to(0);
 	//set_all_DACs_to_built_in_nominal_values();
-	close_pci();
 
+	close_pci();
 	return 0;
 }
 
