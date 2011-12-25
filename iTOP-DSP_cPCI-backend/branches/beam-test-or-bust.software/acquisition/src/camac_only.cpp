d5aec40015d1dd0dec15eeb5fb0c80ad8aaef778
Matt Andrew <mzandrew@users.noreply.github.com>
2011-12-25 21:55:39 +0000

    trying to fix 3377

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/camac_only.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/camac_only.cpp
@@ -19,16 +19,26 @@ int main(void) {
 	spill_number = 1;
 	create_directory_if_necessary(location_of_raw_datafiles);
 	generate_new_base_filename();
-	init_camac("CAMAC_config.txt");
-	CAMAC_initialize_3377s();
-	open_CAMAC_file();
-	open_CAMAC3377_file();
+	if (init_camac("CAMAC_config.txt")) {
+		cerr << "ERROR:  could not connect to CAMAC crate" << endl;
+		exit(7);
+	} else {
+		CAMAC_initialized = true;
+	}
+	if (CAMAC_initialized) {
+		open_CAMAC_file();
+		CAMAC_initialize_3377s();
+		open_CAMAC3377_file();
+	}
+
+//	printf("waiting for spill...\n");
+//	wait_for_start_of_spill();
 
 	printf("starting CAMAC-only readout...\n");
 	for (int i=0; i<total_number_of_quarter_events_to_read_per_fiber_channel; i++) {
+		printf("camac-only event #%03d  ", i);
 		read_data_from_CAMAC_and_write_to_CAMAC_file();
 		CAMAC_read_3377s();
-		printf("read camac-only event #%i\n",i);
 	}
 
 	return 0;
