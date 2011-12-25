47aae3f0b523728eeffa540b81d3c08f6cc4c3d5
Matt Andrew <mzandrew@users.noreply.github.com>
2011-12-25 21:34:53 +0000

    trying to fix 3377

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/camac_only.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/camac_only.cpp
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include "parse_config_file.h"
 #include "acquisition.h"
+#include "status_file.h"
 
 int main(void) {
 	unsigned long int total_number_of_quarter_events_to_read_per_fiber_channel = 100;
