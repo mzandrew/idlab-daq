fd5c2bed9b5fd55da02dd0b82c6ee4d46c0b2dfc
mzandrew <mzandrew@e3e55cb2-ede6-3e39-f4bf-95de71bb434d>
2011-12-28 20:08:48 +0000

    now catches ctrl-c and gracefully finishes logfile and closes all files; removed prompt in prerun.cpp about waiting for fiber links to come up because the latest firmware doesn't need it; removed some cruft from the makefile

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/readout_temperature_of_all_connected_modules.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/readout_temperature_of_all_connected_modules.cpp
@@ -12,13 +12,13 @@ int main(void) {
 	setup_pci(card_id);
 	readout_all_pending_data();
 	setup_filenames_for_fiber();
+	//setup_to_catch_ctrl_c();
 	//open_logfile();
 	open_files_for_all_enabled_fiber_channels();
 
 	readout_N_events(1);
 
-	close_all_fiber_files();
-	close_pci();
+	close_all_files();
 	return 0;
 }
 
--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/window_profiler.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/window_profiler.cpp
@@ -15,6 +15,7 @@ int main(void) {
 	should_soft_trigger = true;
 	readout_all_pending_data();
 	setup_filenames_for_fiber();
+	setup_to_catch_ctrl_c(update_logfile_with_the_number_of_readout_events_for_this_spill_and_close_all_files);
 	open_logfile();
 	open_files_for_all_enabled_fiber_channels();
 
@@ -46,9 +47,8 @@ int main(void) {
 		set_start_and_end_windows( i,  i+3); usleep(10000); readout_N_events(100);
 	}
 
-	close_all_fiber_files();
-	close_pci();
-
+	// cleanup:
+	close_all_files();
 	return 0;
 }
 
