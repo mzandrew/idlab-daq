20692eff4712926d4a52dc381e60ef22a03b063c
mzandrew@gmail.com <mzandrew@gmail.com@e3e55cb2-ede6-3e39-f4bf-95de71bb434d>
2012-08-19 08:12:55 +0000

    implemented Kurtis's fDIRC modifications

--- iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/cosmic.cpp
+++ iTOP-DSP_cPCI-backend/branches/beam-test-or-bust.software/acquisition/src/cosmic.cpp
@@ -17,13 +17,13 @@ int main(void) {
 
 	parse_config_file(".config");
 
-	init_camac();
-	open_CAMAC_file();
+	init_CAMAC_controller();
+//	open_CAMAC_file(); // fDIRC version comments this out
 
 	setup_pci(card_id);
 	wait_for_all_links_to_come_up(channel_bitmask);
 	//should_soft_trigger = false;
-	should_soft_trigger = true;
+//	should_soft_trigger = true; // fDIRC version comments this out
 	readout_all_pending_data();
 	set_event_number(0);
 //	set_all_DACs_to_built_in_nominal_values();
@@ -34,13 +34,17 @@ int main(void) {
 	open_logfile();
 	open_files_for_all_enabled_fiber_channels();
 
+	//set_number_of_windows_to_look_back(32); // fDIRC version has this not commented out
+
 	send_front_end_trigger_veto_clear();
 	reset_trigger_flip_flop();
 
 	for (int i=0; i<total_number_of_quarter_events_to_read_per_fiber_channel; i++) {
 		readout_N_events(1);
 		usleep(1000000);
-		read_data_from_CAMAC_and_write_to_CAMAC_file();
+		if (CAMAC_initialized) {
+			read_data_from_CAMAC_and_write_to_CAMAC_file();
+		}
 //		increment_spill_number_and_change_filenames_for_fiber();
 		disk_space_free(location_of_raw_datafiles);
 	}
