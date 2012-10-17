3d75db05fbb3afb7ef22c0b88e42aa3d81622ec4
craig.bookwalter <craig.bookwalter@gmail.com>
2012-10-17 02:05:53 +0000

    yea

--- iTOP-DSP_FIN-COPPER-FINESSE/branches/crt-nov2012/acquisition/src/lib/DebugInfoWarningError.h
+++ iTOP-DSP_FIN-COPPER-FINESSE/branches/crt-nov2012/acquisition/src/lib/DebugInfoWarningError.h
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-extern unsigned short int verbosity;
 
 void setup_DebugInfoWarningError(void);
 unsigned short int change_verbosity(unsigned short int new_verbosity);
 
-extern FILE *debug;
-extern FILE *debug2;
-extern FILE *info;
-extern FILE *warning;
-extern FILE *error;
 
