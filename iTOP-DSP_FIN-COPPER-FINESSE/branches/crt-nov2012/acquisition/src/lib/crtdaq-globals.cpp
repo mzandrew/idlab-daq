7a600dfd184f4350198b35152c37d27606c47d52
craig.bookwalter@gmail.com <craig.bookwalter@gmail.com@e3e55cb2-ede6-3e39-f4bf-95de71bb434d>
2012-11-02 15:51:05 +0000

    already started a little bit in cprdaq.cpp, but committing before a change to not hold dsp-fin device files for the duration of execution

--- iTOP-DSP_FIN-COPPER-FINESSE/branches/crt-nov2012/acquisition/src/lib/crtdaq-globals.cpp
+++ iTOP-DSP_FIN-COPPER-FINESSE/branches/crt-nov2012/acquisition/src/lib/crtdaq-globals.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sys/types.h>
 
 #include "crtdaq-globals.h" 
 
@@ -19,8 +20,11 @@ const char    *_g_findevpath[4]   = {"/dev/copper/cprdsp-fin:a",
 				     "/dev/copper/cprdsp-fin:b",
 				     "/dev/copper/cprdsp-fin:c",
 				     "/dev/copper/cprdsp-fin:d"};
-unsigned short _g_fin_bitmask     = 0;
-unsigned short _g_fiber_bitmask   = 0;
+unsigned int  _g_fin_bitmask     = 0;
+unsigned int  _g_fiber_bitmask   = 0;
+
+string         _g_loopbacks_requested = "";
+unsigned int   _g_loopback_mask      = 0;
 
 int            _g_nfins_enabled   = 0;
 int            _g_nfibers_enabled = 0;
@@ -43,9 +47,9 @@ unsigned int   _g_feedback_enables_and_goals[6]    = {0,0,0,0,0,0};
 
 char _g_red[13], _g_yellow[13], _g_white[13];
 
-unsigned short _g_threshold_scan_low_limit         = 1310;
-unsigned short _g_threshold_scan_high_limit        = 2620;
-unsigned short _g_threshold_scan_step_size         = 10;
+unsigned int   _g_threshold_scan_low_limit         = 1310;
+unsigned int   _g_threshold_scan_high_limit        = 2620;
+unsigned int   _g_threshold_scan_step_size         = 10;
 string         _g_current_date_string              = "no_date";
 
 int _g_run_number        = 1;
@@ -70,19 +74,24 @@ const char *_g_cprdevpath         = "/dev/copper/copper";
 int         _g_cprdev             = 0;
 int         _g_cprpoll_timeout_ms = 100;
 
+
+const u_int32_t _g_packet_header                       = 0x00be11e2;
+const u_int32_t _g_protocol_freeze_date                = 0x20111213;
+const u_int32_t _g_packet_footer                       = 0x62504944;
+const u_int32_t _g_packet_type[NUMBER_OF_PACKET_TYPES] = { 0x00c0ffee, 
+							   0x0000eada, 
+							   0x000f00da, 
+							   0x000ab0de, 
+							   0xce11b10c };
+u_int32_t       _g_packet[NUMBER_OF_WORDS_IN_A_PACKET];
+
+
 // Filled in DebugInfoWarningError.cpp
 FILE *_g_debug   = 0;
 FILE *_g_debug2  = 0;
 FILE *_g_info    = 0;
 FILE *_g_warning = 0;
 FILE *_g_error   = 0;
-
-//struct DAC_settings_type _g_DAC_settings;
-//char             *_g_command_packet       = new char[SINGLE_PACKET_BUFFER_SIZE_IN_BYTES];
-// unsigned long     _g_packet_header        = 0;
-// unsigned long     _g_protocol_freeze_date = 0;
-// unsigned long     _g_packet_footer        = 0;
-// unsigned long     _g_packet_type[NUMBER_OF_PACKET_TYPES];
 
 
 void crtdaq_default_ctrl_c_handler() {
